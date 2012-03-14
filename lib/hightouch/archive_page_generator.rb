e942f1fb6be969edc64f74de5d2efd59636496cb
Tower He <towerhe@gmail.com>
2012-03-14 12:20:41 +0800

    Fixed archives generation

--- lib/hightouch/archive_page_generator.rb
+++ lib/hightouch/archive_page_generator.rb
@@ -1,7 +1,7 @@
 module Hightouch
   module ArchivePageGenerator
     def generate_archive_page(archive)
-      page archive.path, proxy: "/templates/archive.html", ignore: true do
+      page archive.path, proxy: "/templates/archive.html" do
         @collection = archive.class.name.split(/::/).last.downcase.pluralize.to_sym
         @archive_name = archive.name
       end
